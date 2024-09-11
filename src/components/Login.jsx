edc6e4dd1e1a5abd381b54c7cd47338e5edc754b
Nicolas Kraft <nkraft2018@fau.edu>
2024-09-11 10:30:09 -0400

    updated css
    css styling for login & signup

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -22,14 +22,18 @@ const Login = () => {
     }
 
     return (
-        <div className="centered-container">
-            <div className="centered-content">
+        <div className="login-page">
+            <div className="login-container">
+                <h5 className="login-title">Login</h5>
                 <form onSubmit={(e) => {e.preventDefault(); handleLogin(email, password)}}>
-                    <h5>Login</h5>
-                    <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)}/>
-                    <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
-                    <button type="submit">Login</button>
-                    <p>Don't have an Account?<Link to="/signup">Sign Up</Link></p>
+                    <div className="email">
+                        <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)}/>
+                    </div>
+                    <div className="password">
+                        <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
+                    </div>
+                    <button className="login-button" type="submit">Login</button>
+                    <p className="link-to-login-signup">Don't have an Account?<Link to="/signup"> Sign Up</Link></p>
                 </form>
             </div>
         </div>
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -21,14 +21,18 @@ const SignUp = () => {
     }
 
     return (
-        <div className="centered-container">
-            <div className="centered-content">
+        <div className="login-page">
+            <div className="login-container">
+                <h5 className="login-title">Sign Up</h5>
                 <form onSubmit={(e) => {e.preventDefault(); handleSignUp(email, password)}}>
-                    <h5>Sign Up</h5>
-                    <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-                    <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
-                    <button type="submit">Sign Up</button>
-                    <p>Already have an Account?<Link to="/login"> Login</Link></p>
+                    <div className="email">
+                        <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)}/>
+                    </div>
+                    <div className="password">
+                        <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
+                    </div>
+                    <button className="login-button" type="submit">Sign Up</button>
+                    <p className="link-to-login-signup">Already have an Account?<Link to="/login"> Login</Link></p>
                 </form>
             </div>
         </div>
