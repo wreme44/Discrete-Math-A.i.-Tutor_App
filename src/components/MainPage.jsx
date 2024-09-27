a6ce5d3754013e8bdcebc32bd9e0dd2aee275790
Nicolas Kraft <nkraft2018@fau.edu>
2024-09-27 17:56:42 -0400

    chatbox rendering
    placed button and chatbox rendering top right with smoother transition effects
    
    solved some css issues with extra gaps showing up etc.

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -35,24 +35,21 @@ const MainPage = () => {
                 <LessonsColumn />
             </div>
             <div className="md:col-span-2 bg-gray-800 pl-3 pt-3 pb-2 rounded overflow-y-auto max-h-full md:h-auto h-[500px]">
-                <button className='absolute hidden md:block bg-blue-800 text-white px-2 py-1 rounded' onClick={toggleChatBox}>
+                <button className='absolute hidden md:block top-16 right-5 bg-blue-800 text-white px-2 py-1 rounded' onClick={toggleChatBox}>
                     {isChatVisible ? "Hide Tutor" : "Message Tutor"}
                 </button>
                 <ExercisesPage />
             </div>
-            {isChatVisible && !isSmallScreen ? (
-                <div className={`md:absolute right-1 md:col-span-1 md:w-1/3 w-full rounded overflow-y-auto z-10 border-4 border-black 
-                    ${isChatVisible && !isSmallScreen ? 'opacity-100' : 'opacity-0'}
-                    transition-all duration-700 ease-in-out`}
-                    style={{ height: '89%', marginTop: '65px', pointerEvents: isChatVisible ? 'auto' : 'none' }}
-                >
-                    <ChatBox />
-                </div>
-            ) : (
-                <div className="md:hidden col-span-1 rounded overflow-y-auto max-h-full h-[500px]">
-                    <ChatBox />
-                </div>
-            )}
+            <div className={`rounded overflow-y-auto ${isSmallScreen
+                ? 'md:hidden col-span-1 max-h-full h-[500px]'
+                : `md:absolute right-1 md:col-span-1 md:w-1/3 w-full z-10 border-4 border-black transition-all duration-1000 ease-in-out 
+                ${isChatVisible ? 'max-h-[800px] opacity-100' : 'max-h-0 opacity-0'}`}`}
+                {...(!isSmallScreen && {
+                    style: { height: '85%', marginTop: '100px', pointerEvents: isChatVisible ? 'auto' : 'none' }
+                })}
+            >
+                <ChatBox />
+            </div>
         </div>
     );
 }
