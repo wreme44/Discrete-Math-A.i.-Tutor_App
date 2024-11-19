b11d796e326098a32b3d8dbfe3c5674d3f43cb8b
Nicolas Kraft <nkraft2018@fau.edu>
2024-11-19 17:54:04 -0500

    testing

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['mathlive']
-  },
+//   optimizeDeps: {
+//     include: ['mathlive']
+//   },
   define: {
     'process.env': {},
   },
