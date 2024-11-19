a2e8b4c334103dc9ddf0d4c02c34df21fe441e4f
Nicolas Kraft <nkraft2018@fau.edu>
2024-11-19 16:44:47 -0500

    testing different ways for heroku hosting

--- server/server.mjs
+++ server/server.mjs
@@ -2,17 +2,31 @@ import express from 'express';
 import axios from 'axios';
 import dotenv from 'dotenv';
 import cors from 'cors';
-// import { Stream } from 'openai/streaming.mjs';
+
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// const rootDir = path.resolve(__dirname, '..');
+
 // middlewares
+// app.use(express.static(path.join(rootDir, 'dist')));
+// app.use(express.static(path.join(__dirname, 'dist')));
+
 app.use(cors());
 app.use(express.json({limit: '20mb'}));
 app.use(express.urlencoded({limit: '20mb', extended: true}));
+
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(rootDir, 'dist', 'index.html'));
+// });
+
 // making sure each chunk is a complete json object
 const isValidJSON = (str) => {
 
