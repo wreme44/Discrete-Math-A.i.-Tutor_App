07c64695d909eccc672cd44399cc7a405088aef7
Nicolas Kraft <nkraft2018@fau.edu>
2024-11-19 16:56:03 -0500

    testing

--- server/server.mjs
+++ server/server.mjs
@@ -11,21 +11,21 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-// const rootDir = path.resolve(__dirname, '..');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const rootDir = path.resolve(__dirname, '..');
 
 // middlewares
-// app.use(express.static(path.join(rootDir, 'dist')));
-// app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(rootDir, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use(cors());
 app.use(express.json({limit: '20mb'}));
 app.use(express.urlencoded({limit: '20mb', extended: true}));
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(rootDir, 'dist', 'index.html'));
-// });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(rootDir, 'dist', 'index.html'));
+});
 
 // making sure each chunk is a complete json object
 const isValidJSON = (str) => {
